d972a4a2fa2eadb84632b72e7013c9ce023763f2
k1502432 <k1502432@kcl.ac.uk>
2024-02-23 02:04:33 +0000

    progress further on _process_tags, push win env

--- preprocess.py
+++ preprocess.py
@@ -1,43 +1,58 @@
 from bs4 import BeautifulSoup
-from collections import defaultdict
 import os
 import re
 import time
 import pandas as pd
 
 
-def load_data(txt_path):
+# still produces duplicates
+def _process_tag(tag, data_dict):
+    tag_name = 'P' if tag.name.startswith('P') else tag.name
+
+    if not re.match(r'(TEI|text|T\d+|U\d+)', tag_name):
+        if tag_name not in data_dict:
+            data_dict[tag_name] = []
+
+        text = ' '.join(tag.text.strip().split())
+        data_dict[tag_name].append(text)
+
+    children = tag.find_all(recursive=False)
+    for child in children:
+        _process_tag(child, data_dict)
+        
+
+def load_data(xml_path):
     start_time = time.time()
     print(f'Started at {time.ctime(start_time)}')
-    csv_path = re.sub(r'\.txt$', '.csv', txt_path)
+    csv_path = re.sub(r'\.xml$', '.csv', xml_path)
+    print
     # check if a csv exists
-    if os.path.exists(csv_path):
-        print('Loading from csv...')
-        df = pd.read_csv(csv_path)
-        return df.to_dict('list')
+    # if os.path.exists(csv_path):
+    #     print('Loading from csv...')
+    #     df = pd.read_csv(csv_path)
+    #     return df.to_dict('list')
     
     print('No csv found, creating one...')
     # load the txt
-    with open(txt_path, 'r', encoding='ISO-8859-1') as f:
+    with open(xml_path, 'r', encoding='ISO-8859-1') as f:
         # total_chars = os.path.getsize(txt_path)
         # fraction = total_chars // 10
         # content = f.read(fraction)
         content = f.read()
 
-    soup = BeautifulSoup(content, 'lxml-xml')
-    
-    data_defaultdict = defaultdict(list)
+    soup = BeautifulSoup(content, 'xml')
+
+    data_dict = {}
+    print(f'len(soup.find_all()): {len(soup.find_all())}')
     for tag in soup.find_all():
-        if not any(tag.name.lower().startswith(prefix) for prefix in ['body', 'text', 't', 'u', 'p']):
-            text = ' '.join(tag.text.strip().split())
-            data_defaultdict[tag.name].append(text)
+        _process_tag(tag, data_dict)
+
+    print(f'len(data_dict): {len(data_dict)}')
 
     # pad shorter lists in data_defaultdict with None
-    max_len = max(len(lst) for lst in data_defaultdict.values())
-    for key in data_defaultdict:
-        data_defaultdict[key] += [None] * (max_len - len(data_defaultdict[key]))
-
-    data_dict = dict(data_defaultdict)
+    max_len = max(len(lst) for lst in data_dict.values())
+    for key in data_dict:
+        data_dict[key] += [None] * (max_len - len(data_dict[key]))
 
     df = pd.DataFrame(data_dict)
 
@@ -50,4 +65,4 @@ def load_data(txt_path):
     return data_dict
 
 
-load_data("data/Tagged ST VW 1927.txt")
+load_data("data/Tagged ST VW 1927.xml")
