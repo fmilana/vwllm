569a2d92fffc5181143192c2e1df58b33ac3afda
k1502432 <k1502432@kcl.ac.uk>
2024-02-27 20:02:41 +0000

    push changes

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+from stringprep import in_table_d2
 from lxml import etree
 import re
 import pandas as pd
@@ -6,7 +7,7 @@ from datasets import Dataset
 from sklearn.model_selection import train_test_split, StratifiedGroupKFold
 
 
-ELEMENT_TAG = 'SC'
+ELEMENT_TAG = 'IIM'
 
 with open('data/Tagged ST VW 1927.xml', 'r', encoding='ISO-8859-1') as f:
     xml_content = f.read()
@@ -34,8 +35,8 @@ def _process_element(element, paragraph_id, result_list, inside=False):
     if element.text:
         words = element.text.split()
         for i, word in enumerate(words):
-            bio_tag = f'B-{ELEMENT_TAG}' if i == 0 and inside else (f'I-{ELEMENT_TAG}' if inside else 'O')
-            result_list.append({'paragraph_id': paragraph_id, 'word': word, 'bio_tag': bio_tag})
+            iob_tag = f'B-{ELEMENT_TAG}' if i == 0 and inside else (f'I-{ELEMENT_TAG}' if inside else 'O')
+            result_list.append({'paragraph_id': paragraph_id, 'word': word, 'iob_tag': iob_tag})
     # process the children of the current element
     for child in element:
         _process_element(child, paragraph_id, result_list, inside)
@@ -43,36 +44,38 @@ def _process_element(element, paragraph_id, result_list, inside=False):
     if element.tail:
         tail_words = element.tail.split()
         for word in tail_words:
-            result_list.append({'paragraph_id': paragraph_id, 'word': word, 'bio_tag': 'O'})
+            result_list.append({'paragraph_id': paragraph_id, 'word': word, 'iob_tag': 'O'})
+            
 
+def _tokenize_and_preserve_labels(batch, label_all_tokens=True):
+    tokenized_inputs = tokenizer(batch['paragraph'], truncation=True, padding=True)
 
-def _tokenize_and_align_labels(examples):
-    tokenized_inputs = tokenizer(examples['word'], truncation=True, padding='max_length', max_length=10)
-    
-    labels = []
-    for i, label in enumerate(examples['bio_tag']):
+    label_ids_list = [] 
+    for i, word_labels in enumerate(batch['word_labels']):
         word_ids = tokenized_inputs.word_ids(batch_index=i)
-        previous_word_idx = None
+        word_labels = word_labels.split(',')
+        
         label_ids = []
+        previous_word_idx = None
         for word_idx in word_ids:
-            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-            # ignored in the loss function.
             if word_idx is None:
                 label_ids.append(-100)
-            # We set the label for the first token of each word. (this token is the first token of the word)
-            elif word_idx != previous_word_idx:
-                label_ids.append(label)
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # whether we are inside a {ELEMENT_TAG} entity or not.
             else:
-                label_ids.append(label if label != 0 else -100)
+                label = word_labels[word_idx] if word_idx < len(word_labels) else 'O'  # Default to 'O' or another default label
+                label_id = label2id[label]
+                # If label_all_tokens is True OR this token is the first token of a word, assign the actual label ID
+                if label_all_tokens or word_idx != previous_word_idx:
+                    label_ids.append(label_id)
+                # Otherwise, if it's a subsequent token of a word and label_all_tokens is False, set its label to -100
+                else:
+                    label_ids.append(-100)
             previous_word_idx = word_idx
-        
-        labels.append(label_ids)
-    
-    tokenized_inputs['labels'] = labels
+
+        label_ids_list.append(label_ids)  # Append the list of label IDs for this sentence to the overall list
+
+    tokenized_inputs["labels"] = label_ids_list
     return tokenized_inputs
-            
+
 
 print('Parsing XML...')
 root = etree.fromstring(xml_content, parser=etree.XMLParser(recover=True))
@@ -84,21 +87,29 @@ _find_paragraphs_and_process(root, result_list)
 
 df = pd.DataFrame(result_list)
 
-df.to_csv(f'data/Tagged ST VW 1927_{ELEMENT_TAG}.csv', index=False)
-print(f'File data/Tagged ST VW 1927_{ELEMENT_TAG}.csv saved')
-
 # split the dataset into train and test sets using StratifiedGroupKFold
 stratified_group_kfold = StratifiedGroupKFold(n_splits=5)
 
-label_map = {'B-SC': 0, 'I-SC': 1, 'O': 2}
-# convert the 'bio_tag' column to integer type
-df['bio_tag'] = df['bio_tag'].map(label_map)
-
-for train_index, test_index in stratified_group_kfold.split(df, df['bio_tag'], df['paragraph_id']):
-    train_df = df.iloc[train_index]
-    test_df = df.iloc[test_index]
+for train_index, test_index in stratified_group_kfold.split(df, df['iob_tag'], df['paragraph_id']):
+    train_df = df.iloc[train_index].copy()
+    test_df = df.iloc[test_index].copy()
     # break the loop after the first iteration
     break
+
+
+train_df['paragraph'] = train_df[['paragraph_id', 'word', 'iob_tag']].groupby(['paragraph_id'])['word'].transform(lambda x: ' '.join(x))
+train_df['word_labels'] = train_df[['paragraph_id', 'word', 'iob_tag']].groupby(['paragraph_id'])['iob_tag'].transform(lambda x: ','.join(x))
+train_df = train_df[['paragraph', 'word_labels']].drop_duplicates().reset_index(drop=True)
+
+test_df['paragraph'] = test_df[['paragraph_id', 'word', 'iob_tag']].groupby(['paragraph_id'])['word'].transform(lambda x: ' '.join(x))
+test_df['word_labels'] = test_df[['paragraph_id', 'word', 'iob_tag']].groupby(['paragraph_id'])['iob_tag'].transform(lambda x: ','.join(x))
+test_df = test_df[['paragraph', 'word_labels']].drop_duplicates().reset_index(drop=True)
+
+train_df.to_csv(f'data/train_{ELEMENT_TAG}.csv', index=False)
+test_df.to_csv(f'data/test_{ELEMENT_TAG}.csv', index=False)
+
+label2id = {f'B-{ELEMENT_TAG}': 0, f'I-{ELEMENT_TAG}': 1, 'O': 2}
+id2label = {0: f'B-{ELEMENT_TAG}', 1: f'I-{ELEMENT_TAG}', 2: 'O'}
 
 train_dataset = Dataset.from_pandas(train_df)
 test_dataset = Dataset.from_pandas(test_df)
@@ -108,8 +119,8 @@ model_name = "google-bert/bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 print('Tokenizing and encoding...')
-tokenized_train_dataset = train_dataset.map(_tokenize_and_align_labels, batched=True)
-tokenized_test_dataset = test_dataset.map(_tokenize_and_align_labels, batched=True)
+tokenized_train_dataset = train_dataset.map(_tokenize_and_preserve_labels, batched=True)
+tokenized_test_dataset = test_dataset.map(_tokenize_and_preserve_labels, batched=True)
 
 tokenized_train_dataset.save_to_disk(f'data/tokenized_train_dataset_{ELEMENT_TAG}.hf')
 tokenized_test_dataset.save_to_disk(f'data/tokenized_test_dataset_{ELEMENT_TAG}.hf')
--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 # https://huggingface.co/docs/transformers/en/training
 from datasets import load_dataset
-from transformers import TrainingArguments, Trainer, AutoModelForTokenClassification 
+import torch
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+from transformers import BertTokenizer, BertForTokenClassification, TrainingArguments, Trainer
+from seqeval.metrics import classification_report, accuracy_score, f1_score, precision_score, recall_score
 
 
-ELEMENT_TAG = 'SC'
+ELEMENT_TAG = 'IIM'
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 model_name = 'google-bert/bert-base-uncased'
 
@@ -12,26 +18,71 @@ test_dataset_path = f'data/tokenized_test_dataset_{ELEMENT_TAG}.hf/data-00000-of
 
 dataset = load_dataset('arrow', data_files={'train': train_dataset_path, 'test': test_dataset_path})
 
-model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=3)
+label2id = {f'B-{ELEMENT_TAG}': 0, f'I-{ELEMENT_TAG}': 1, 'O': 2}
+id2label = {0: f'B-{ELEMENT_TAG}', 1: f'I-{ELEMENT_TAG}', 2: 'O'}
+
+
+# def compute_metrics(p):
+#     predictions = np.argmax(p.predictions, axis=-1)
+#     true_labels = p.label_ids
+
+#     # Convert indices to labels, ignoring -100 labels
+#     pred_labels = [[id2label[p] for p, l in zip(pred, lbl) if l != -100] for pred, lbl in zip(predictions, true_labels)]
+#     true_labels = [[id2label[l] for p, l in zip(pred, lbl) if l != -100] for pred, lbl in zip(predictions, true_labels)]
+
+#     # Calculate metrics per tag
+#     report = classification_report(true_labels, pred_labels)
+
+#     return report
+
+model = BertForTokenClassification.from_pretrained('bert-base-uncased', num_labels=len(id2label), id2label=id2label, label2id=label2id)
+
+# sanity check (initial_loss should close to ln(3) = 1.0986122886681098)
+# ids = torch.tensor(dataset['train'][0]['input_ids']).unsqueeze(0)
+# mask = torch.tensor(dataset['train'][0]['attention_mask']).unsqueeze(0)
+# labels = torch.tensor(dataset['train'][0]['labels']).unsqueeze(0)
+# ids = ids.to('cuda')
+# mask = mask.to('cuda')
+# labels = labels.to('cuda')
+# outputs = model(ids, mask, labels=labels)
+# initial_loss = outputs.loss
+# print(f'initial_loss = {initial_loss}')
+# # sanity check (tr_logits.shape should be (1, 512, 3))
+# tr_logits = outputs.logits
+# print(f'tr_logits.shape = {tr_logits.shape}')
 
 training_args = TrainingArguments(
-    output_dir='checkpoints', 
-    num_train_epochs=3,
+    output_dir=f'checkpoints/{ELEMENT_TAG}', 
+    num_train_epochs=8,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
     warmup_steps=500,
     weight_decay=0.01,
-    evaluation_strategy='epoch'
+    learning_rate=1e-05,
+    evaluation_strategy='epoch',
+    logging_strategy='epoch',
+    logging_steps=100,
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=dataset['train'],
-    eval_dataset=dataset['test']
+    eval_dataset=dataset['test'],
+    # compute_metrics=compute_metrics
 )
 
 trainer.train()
 
-results = trainer.evaluate()
-print(results)
+report = trainer.evaluate()
+
+print(report)
+
+
+# # to-do:
+# omit [pad] tokens 
+
+
+# # make sure not to overfit, use train test validate
+
+# # Class weights: You can assign higher weights to the minority class during training. In the Hugging Face Trainer, you can do this by setting the compute_loss function to use class weights.
